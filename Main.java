fb323d71329b24cf6ec2655e0e6474f453510a87
Eloisa Reis <reiseloisa7@gmail.com>
2024-09-03 20:17:59 -0300

    Adicionado Main - adicionado Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Questao leia = new Questao();
+
+        String[] perguntas = {
+                "01. Quem é a única mulher que apareceu na foto: \"reunião dos maiores cientistas do mundo\" : \"A Foto Mais \n Inteligente do Mundo\"?"
+                "02. Qual é o maior planeta do Sistema Solar?"
+        }
+
+        String[][] opcoes = {
+                ""
+                ""
+        }
+
+        String[] correta {
+            "c"
+        }
+
+        for (int i = 0; i < 15; i++) {
+            leia.pergunta
+        }
+    }
+}
