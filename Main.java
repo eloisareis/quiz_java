8f85f915b42a3e36b1dda15a7862044f1d7bd833
Eloisa Reis <reiseloisa7@gmail.com>
2024-09-05 14:36:55 -0300

    Segundo commit Main - alterado Main.java

--- Main.java
+++ Main.java
@@ -3,21 +3,29 @@ public class Main {
         Questao leia = new Questao();
 
         String[] perguntas = {
-                "01. Quem é a única mulher que apareceu na foto: \"reunião dos maiores cientistas do mundo\" : \"A Foto Mais \n Inteligente do Mundo\"?"
+                "01. Quem é a única mulher que apareceu na foto: \"reunião dos maiores cientistas do mundo\" : \"A Foto Mais \n Inteligente do Mundo\"?",
                 "02. Qual é o maior planeta do Sistema Solar?"
-        }
+        };
 
         String[][] opcoes = {
-                ""
-                ""
-        }
+                {"a. Caroline Herschel", "b. Rosalind Franklin", "c. Marie Curie", "d. Margaret Heafield", "e. Valentina Tereshkova"},
+                {"a. Urano", "b. Júpiter", "c. Terra", "d. Vênus", "e. Marte"}
+        };
 
-        String[] correta {
-            "c"
-        }
+        String[] corretas = {"c", "b"};
 
         for (int i = 0; i < 15; i++) {
-            leia.pergunta
+            leia.pergunta = perguntas[i][0];
+            leia.opcaoA   = opcoes[i][1];
+            leia.opcaoB   = opcoes[i][2];
+            leia.opcaoC   = opcoes[i][3];
+            leia.opcaoD   = opcoes[i][4];
+            leia.opcaoE   = opcoes[i][5];
+            leia.correta  = corretas[i];
+
+            leia.escrevaQuestao();
+            String resposta = leia.leiaResposta();
+            Boolean correta = leia.isCorreta(resposta);
         }
     }
 }
